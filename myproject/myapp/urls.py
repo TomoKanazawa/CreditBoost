45260b554ded13ce6bb9b7cdec778454f624e8eb
Young <katelyn.l.young.96@gmail.com>
2023-05-28 00:00:59 -0700

    add loan application and manage activate loan pages

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -7,7 +7,9 @@ app_name = 'myapp'
 urlpatterns = [
   path('', views.index, name='index'),
   path('profile', views.profile, name='profile'),
-  path('loanmanage', views.loanmanage, name='loanmanage'),
+  path('loan_application', views.loan_application, name='loan_application'),
+  path('manage_active_loan', views.manage_active_loan, name='manage_active_loan'),
+  path('loan_manage', views.loan_manage, name='loan_manage'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,5 +7,11 @@ def index(request):
 def profile(request):
     return render(request, 'myapp/profile.html')
 
-def loanmanage(request):
-    return render(request, 'myapp/loanmanage.html')
+def loan_manage(request):
+    return render(request, 'myapp/loan_manage.html')
+
+def loan_application(request):
+    return render(request, 'myapp/loan_application.html')
+
+def manage_active_loan(request):
+    return render(request, 'myapp/manage_active_loan.html')
