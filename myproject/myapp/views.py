2dca0e228a9c0cb0963807dd80910a712b5f7746
Young <katelyn.l.young.96@gmail.com>
2023-05-27 22:22:00 -0700

    added profile and loanmanage pages

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'myapp/index.html')
+
+def profile(request):
+    return render(request, 'myapp/profile.html')
+
+def loanmanage(request):
+    return render(request, 'myapp/loanmanage.html')
