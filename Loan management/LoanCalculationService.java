ab9d21176c5d07a084b446076f180829c1b21c59
StormYuchen <126362263+StormYuchen@users.noreply.github.com>
2023-05-31 04:21:07 +0800

    Add files via upload

---
+++ Loan management/LoanCalculationService.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanCalculationService {
+    public double calculateLoanAmount(Long userId) {
+        // Call the algorithm to calculate the latest loan amount
+        // Return the latest loan amount
+    }
+}
