351f527f3a37d7460e65fed36990def907038c0f
StormYuchen <126362263+StormYuchen@users.noreply.github.com>
2023-05-31 11:54:04 +0800

    Delete LoanCalculationService.java

--- Loan management/LoanCalculationService.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.stereotype.Service;
-
-@Service
-public class LoanCalculationService {
-    public double calculateLoanAmount(Long userId) {
-        // Call the algorithm to calculate the latest loan amount
-        // Return the latest loan amount
-    }
-}
